742d19214330b4ac46e5757a9932d456bef135f9
aakzsh <69726390+aakzsh@users.noreply.github.com>
2021-06-12 11:28:01 +0530

    commit

--- src/components/Visualise/Visualise.js
+++ src/components/Visualise/Visualise.js
@@ -1,9 +1,14 @@
 import Nav from "../../components/Nav.js";
+import NavVert from "../../components/Nav-vert.js";
 const Visualise = ()=>{
 
     return <>
     <Nav/>
+    <NavVert/>
+    <canvas/>
+    
     </>;
 }
 
 export default Visualise;
+
